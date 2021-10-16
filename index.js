4701fb7aae4692f048b87abac842f922651b8ead
Akshay Jangir <>
2021-10-16 17:57:28 +0000

    changes

---
+++ index.js
@@ -0,0 +1,16 @@
+var rs = require("readline-sync")
+let chalk = require('chalk')
+
+let log = console.log
+
+let blueBright = chalk.bold.blueBright
+let green = chalk.bold.green
+let red = chalk.bold.red
+let cyan = chalk.bold.cyan
+let yellow = chalk.bold.yellow
+let title = chalk.black.bold.bgYellow
+
+var name = ""
+var score = 0
+var questionList = []
+var scoreboard = []
