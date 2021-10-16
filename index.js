f946db7d747d994142b081361e7b10d5494f928d
Akshay Jangir <52463851+axy1976@users.noreply.github.com>
2021-10-16 18:32:08 +0000

    commited

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ var rs = require("readline-sync")
 let chalk = require('chalk')
 
 let log = console.log
+let score = 0;
+let ch = "";
 
-let blueBright = chalk.bold.blueBright
-let green = chalk.bold.green
-let red = chalk.bold.red
-let cyan = chalk.bold.cyan
-let yellow = chalk.bold.yellow
-let title = chalk.black.bold.bgYellow
+let user = rs.question(chalk.yellow("Player Name | "));
 
-var name = ""
-var score = 0
-var questionList = []
-var scoreboard = []
+log("1 > In the following which one is not an Anime series ? ");
+log("(a) Doremon         (b) Naruto");
+log("(c) One Piece       (d) Baruto");
 
+ch = rs.question("Answer > ");
+if(ch == "a")
+{
+  score += 5;
+}
+else
+{
+  score -= 2;
+}
 
+log(user +" | "+score);
